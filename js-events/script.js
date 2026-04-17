f19b6d9e5670783252f07696a292ee76bff67ec3
Rodrigo <rlopezca89@gmail.com>
2026-04-17 17:29:03 +0000

    class 21

---
+++ js-events/script.js
@@ -0,0 +1,14 @@
+// function greet(name){
+//     //console.log(`Hello ${name}`);
+//     console.log("hello"+ name)
+// }
+
+// greet("Miku");
+
+// function addnumbers(num1, num2){
+//     let sum = num1 + num2;
+//     alert("Sum is equal to " + sum);
+// }
+function Article1(){
+    one = "
+}
