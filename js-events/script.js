aad2f2287adceffe44046eb739671f8915922aeb
Rodrigo <rlopezca89@gmail.com>
2026-04-20 17:29:02 +0000

    class 22

--- js-events/script.js
+++ js-events/script.js
@@ -9,6 +9,15 @@
 //     let sum = num1 + num2;
 //     alert("Sum is equal to " + sum);
 // }
-function Article1(){
-    one = "
+function Articleone(){
+    let one = "We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.";
+    alert("The Article one is: " + one);
+}
+function Articletwo(){
+    let two = "We believe that men will be punished for their  own sins, and not for Adam's transgression.";
+    alert("The Article two is: " +two);
+}
+function Articlethree(){
+   let three = "We believe that through the Atonement of Christ, all mankind may be save, by obedience to the laws and oridnances of the Gospel.";
+    alert("The Article three is: " +three);
 }
