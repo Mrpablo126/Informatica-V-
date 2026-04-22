a9615170f3fd6b39338accb51847d78f7d235d7e
Rodrigo <rlopezca89@gmail.com>
2026-04-22 20:19:02 +0000

    class 23

---
+++ js-variables/script.js
@@ -0,0 +1,16 @@
+function one(){
+    const firstName = "Rodrigo";
+    const favoriteColor = "blue";
+    let location = "Salon Profe Calahorrita";
+    let mood = "Tired";
+    console
+}
+function two(){
+    const
+}
+function three(){
+    let
+}
+function four(){
+    let
+}
